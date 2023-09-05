b7d1c42cacc5bb4ee4661efbe9e686c9c8d373ad
marcelosnows <marcsneves@hotmail.com>
2023-09-05 18:51:38 -0300

    Created SignIn Page

---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+import backgroundImg from '../../assets/SignInBackground.png';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Form = styled.form`
+  padding: 0 136px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+
+  > h1 {
+    font-size: 48px;
+    color: ${({ theme }) => theme.COLORS.ORANGE}
+  };
+
+  > h2 {
+    font-size: 24px;
+    margin-top: 84px;
+    margin-bottom: 24px;
+  };
+
+  > p {
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  };
+
+  > a {
+    margin-top: 124px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};   
+  };
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${backgroundImg});
+  background-repeat: no-repeat;
+  background-position: center;
+  align-content: center;
+  background-size: cover;
+`;
